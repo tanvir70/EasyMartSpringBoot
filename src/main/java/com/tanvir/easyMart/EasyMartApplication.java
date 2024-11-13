b6c7dee2abb59c4bb6f2693360784fea792728d9
tanvir70 <rifattanvir70@gmail.com>
2024-11-14 01:25:38 +0600

    Stating the EasyMart Spring Boot conversion

---
+++ src/main/java/com/tanvir/easyMart/EasyMartApplication.java
@@ -0,0 +1,13 @@
+package com.tanvir.easyMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanvir/easyMart/EasyMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanvir.easyMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
