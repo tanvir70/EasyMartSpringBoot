2b8b3d113647c67e82d9c838c4ed44fd3fb60baf
tanvir70 <rifattanvir70@gmail.com>
2024-11-16 03:40:39 +0600

    building category create add delete

--- src/main/java/com/tanvir/easyMart/controller/CategoryController.java
+++ src/main/java/com/tanvir/easyMart/controller/CategoryController.java
@@ -1,4 +1,42 @@
 package com.tanvir.easyMart.controller;
 
+import com.tanvir.easyMart.model.Category;
+import com.tanvir.easyMart.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+
+import java.util.List;
+
+@RestController
 public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("api/public/categories")
+    public String CreateCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "Category created";
+    }
+
+    @DeleteMapping("api/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
 }
--- src/main/java/com/tanvir/easyMart/service/CategoryService.java
+++ src/main/java/com/tanvir/easyMart/service/CategoryService.java
@@ -1,4 +1,13 @@
 package com.tanvir.easyMart.service;
 
-public class CategoryService {
+import com.tanvir.easyMart.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
 }
