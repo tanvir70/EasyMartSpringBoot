08f8beaeb4abceb5e8f189f510617ce30032a44d
tanvir70 <rifattanvir70@gmail.com>
2024-11-17 01:32:39 +0600

    annotated entity for database table creation.

--- src/main/java/com/tanvir/easyMart/model/Category.java
+++ src/main/java/com/tanvir/easyMart/model/Category.java
@@ -1,11 +1,28 @@
 package com.tanvir.easyMart.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "categories")
 public class Category {
+    @Id
     private Long categoryId;
     private String categoryName;
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public Category() {
+
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
 
@@ -15,13 +32,5 @@ public class Category {
 
     public void setCategoryId(Long categoryId) {
         this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
     }
 }
