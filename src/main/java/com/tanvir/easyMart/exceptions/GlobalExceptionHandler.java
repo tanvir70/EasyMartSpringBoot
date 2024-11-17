890ecf9046b289c52b4da26d3217c953de288d42
tanvir70 <rifattanvir70@gmail.com>
2024-11-18 04:37:15 +0600

    forgot to add annotation, also rename file due to conflict with annotation name.

--- src/main/java/com/tanvir/easyMart/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tanvir/easyMart/exceptions/GlobalExceptionHandler.java
@@ -4,15 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.lang.reflect.Field;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> myMethodArgumentNotValid (MethodArgumentNotValidException exception) {
         Map<String,String> response = new HashMap<>();
         exception.getBindingResult().getAllErrors().forEach(error -> {
