f6a54c621b5c522abfa3ea0db3c36161d918812a
tanvir70 <rifattanvir70@gmail.com>
2024-11-18 04:40:39 +0600

    Adding more validation

--- src/main/java/com/tanvir/easyMart/model/Category.java
+++ src/main/java/com/tanvir/easyMart/model/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     @NotBlank
+    @Size(min = 2, max = 20,message = "Category name must be under 20 characters")
     private String categoryName;
 
 /*    public Category(Long categoryId, String categoryName) {
