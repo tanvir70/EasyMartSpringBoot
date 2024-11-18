c78f6876ef114e08fd267d9f98c3969544b66f06
tanvir70 <rifattanvir70@gmail.com>
2024-11-19 01:50:10 +0600

    No change.

--- src/main/java/com/tanvir/easyMart/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tanvir/easyMart/exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
