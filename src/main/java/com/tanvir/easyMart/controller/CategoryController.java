e75302a33395a425ef570c53f22730efe0f86cab
tanvir70 <rifattanvir70@gmail.com>
2024-11-19 02:19:30 +0600

    Changes by bazlur

--- src/main/java/com/tanvir/easyMart/controller/CategoryController.java
+++ src/main/java/com/tanvir/easyMart/controller/CategoryController.java
@@ -6,9 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 
 import java.util.List;
--- src/main/java/com/tanvir/easyMart/service/CategoryService.java
+++ src/main/java/com/tanvir/easyMart/service/CategoryService.java
@@ -7,9 +7,10 @@ import java.util.List;
 public interface CategoryService {
 
     List<Category> getAllCategories();
-    void createCategory(Category category);
+
+    Category createCategory(Category category);
 
     String  deleteCategory(Long categoryId);
 
-    String updateCategory(Long categoryId, Category category);
+    Category updateCategory(Long categoryId, Category category);
 }
